0beed904652fc494a2fc70d12a6790aa631935ad
igorsiwek <igor.siwek94@gmail.com>
2022-04-26 20:36:20 +0200

    20:36

--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -2,28 +2,42 @@ import java.util.Scanner;
 public class BankApp {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner (System.in);
-        Account account = new Account ();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello in our bank");
+        Account account = new Account();
 
         account.createAccount();
-        System.out.println("Hello in our bank");
-        System.out.println("1. Display account details");
-        System.out.println("2. Deposit money");
-        System.out.println("3. Withdrawal");
-        System.out.println("Select number");
+
 
         int inputNumber;
-        inputNumber = scanner.nextInt();
-
-        switch (inputNumber){
-            case 1:
-                account.showAccount();
-            case 2:
-                account.depositMoney();
-            case 3:
-                account.withdrawal();
-
+        do {
+            System.out.println("1. Display account details");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Withdrawal");
+            System.out.println("4. Exit");
+            System.out.println("Select number");
+            inputNumber = scanner.nextInt();
+            switch (inputNumber) {
+                case 1:
+                    account.showAccount();
+                    break;
+                case 2:
+                    account.depositMoney();
+                    break;
+                case 3:
+                    account.withdrawal();
+                    break;
+                case 4:
+                    System.out.println("see you");
+            }
 
         }
+
+        while (inputNumber != 4);
+
     }
 }
+
+
+
+
